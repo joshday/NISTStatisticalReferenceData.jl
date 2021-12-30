f615d11934b986b73d9e3e02a0cf0539b0478051
joshday <emailjoshday@gmail.com>
2021-12-30 11:51:20 -0500

    all certified values working, add readme docs

--- README.md
+++ README.md
@@ -1,1 +1,175 @@
 # NISTStatisticalReferenceData
+
+This package provides utilities for working with the datasets and certified values provided by
+[NIST's Statistical Reference Datasets (StRD)](https://www.itl.nist.gov/div898/strd/).
+
+Taken directly from the NIST StRD website:
+
+> The purpose of this project is to improve the accuracy of statistical software 
+> by providing reference datasets with certified computational results that enable 
+> the objective evaluation of statistical software.
+
+## Which statistical analyses can be tested with this package?
+
+NIST StRD provides example datasets and certified statistical results for:
+
+- One-Way ANOVA
+- MCMC
+- Nonlinear Regression
+- Linear Regression
+- Univariate Statistics
+
+To list the available datasets, use the `datasets()` function:
+
+```julia
+julia> NISTStatisticalReferenceData.datasets()
+OrderedCollections.OrderedDict{String, Vector{String}} with 5 entries:
+  "anova"      => ["AtmWtAg", "SiRstv", "SmLs01", "SmLs02", "SmLs03", "SmLs04", "SmLs05", "SmLs06", "SmLs07", "SmLs08", "SmLs09"]
+  "mcmc"       => ["mcmc01", "mcmc02", "mcmc03", "mcmc04", "mcmc05", "mcmc06"]
+  "nonlinear"  => ["Bennett5", "BoxBOD", "Chwirut1", "Chwirut2", "DanWood", "ENSO", "Eckerle4", "Gauss1", "Gauss2", "Gauss3",  …] 
+  "regression" => ["Filip", "Longley", "NoInt1", "NoInt2", "Norris", "Pontius", "Wampler1", "Wampler2", "Wampler3", "Wampler4", "Wampler5"]
+  "univariate" => ["Lew", "Lottery", "Mavro", "Michelsoved", "NumAcc1", "NumAcc2", "NumAcc3", "NumAcc4", "PiDigitsved"]
+```
+
+## Loading data
+
+Use `TestCase(dataset)` to load the dataset, its metadata, and the certified values.
+
+```julia
+julia> NISTStatisticalReferenceData.TestCase("Lottery")
+Test Case
+    LocalFile: /Users/joshday/.julia/dev/NISTStatisticalReferenceData/deps/univariate/Lottery.dat
+    URL      : https://www.itl.nist.gov/div898/strd/univ/data/Lottery.dat
+    Procedure: Univariate
+    Data Description: 
+        "Real World"
+        1    Response          : y = 3-digit random number
+        0    Predictors
+        218  Observations
+    Model: 
+        Lower Level of Difficulty
+        2    Parameters        : mu, sigma
+        1    Response Variable : y
+        0    Predictor Variables
+    Data (CSV): 
+        219 Lines
+    Certified Values:
+        UnivariateStats: m=518.95871559633, s=291.699727470969, a=-0.120948622967393, n=218 | exact=false
+```
+
+## How this package works 
+
+- All the NIST StRD's `.dat` files are downloaded in this package's `deps/build.jl` script.
+- The values of `TestCase(dataset)` are populated from the `.dat` files.
+  - For example, here is the `AtmWtAg.dat` file (for ANOVA):
+
+```
+NIST/ITL StRD 
+Dataset Name:   AtmWtAg   (AtmWtAg.dat)
+
+
+File Format:    ASCII
+                Certified Values   (lines 41 to 47)
+                Data               (lines 61 to 108) 
+
+
+Procedure:      Analysis of Variance
+
+
+Reference:      Powell, L.J., Murphy, T.J. and Gramlich, J.W. (1982).
+                "The Absolute Isotopic Abundance & Atomic Weight
+                of a Reference Sample of Silver".
+                NBS Journal of Research, 87, pp. 9-19.
+
+
+Data:           1 Factor
+                2 Treatments
+                24 Replicates/Cell
+                48 Observations
+                7 Constant Leading Digits
+                Average Level of Difficulty
+                Observed Data
+
+
+Model:          3 Parameters (mu, tau_1, tau_2)
+                y_{ij} = mu + tau_i + epsilon_{ij}
+
+
+
+
+
+
+Certified Values:
+
+Source of                  Sums of               Mean               
+Variation          df      Squares              Squares             F Statistic
+
+
+Between Instrument  1 3.63834187500000E-09 3.63834187500000E-09 1.59467335677930E+01
+Within Instrument  46 1.04951729166667E-08 2.28155932971014E-10
+
+                   Certified R-Squared 2.57426544538321E-01
+
+                   Certified Residual
+                   Standard Deviation  1.51048314446410E-05
+
+
+
+
+
+
+
+
+
+
+
+Data:  Instrument           AgWt
+           1            107.8681568
+           1            107.8681465
+           1            107.8681572
+           1            107.8681785
+           1            107.8681446
+           1            107.8681903
+           1            107.8681526
+           1            107.8681494
+           1            107.8681616
+           1            107.8681587
+           1            107.8681519
+           1            107.8681486
+           1            107.8681419
+           1            107.8681569
+           1            107.8681508
+           1            107.8681672
+           1            107.8681385
+           1            107.8681518
+           1            107.8681662
+           1            107.8681424
+           1            107.8681360
+           1            107.8681333
+           1            107.8681610
+           1            107.8681477
+           2            107.8681079
+           2            107.8681344
+           2            107.8681513
+           2            107.8681197
+           2            107.8681604
+           2            107.8681385
+           2            107.8681642
+           2            107.8681365
+           2            107.8681151
+           2            107.8681082
+           2            107.8681517
+           2            107.8681448
+           2            107.8681198
+           2            107.8681482
+           2            107.8681334
+           2            107.8681609
+           2            107.8681101
+           2            107.8681512
+           2            107.8681469
+           2            107.8681360
+           2            107.8681254
+           2            107.8681261
+           2            107.8681450
+           2            107.8681368
+```
--- src/NISTStatisticalReferenceData.jl
+++ src/NISTStatisticalReferenceData.jl
@@ -1,20 +1,348 @@
 module NISTStatisticalReferenceData
 
 using Downloads
+using OrderedCollections: OrderedDict
+using CSV
+using DataFrames
+using Parsers
 
-function parse_dat(file::String)
-    lines = filter(!isempty, readlines(file))
+depsdir(args...) = abspath(joinpath(@__DIR__, "..", "deps", args...))
 
-    name = split(filter(startswith("Dataset Name:"), lines)[1])[3]
-
-    format_row = findfirst(startswith("File Format:"), lines)
-    format = split(lines[format_row])[3]
-
-    certified_values = split(lines[format_row + 1])[[4,6]]
-    certified_values_from = parse(Int, certified_values[1])
-    certified_values_to = parse(Int, replace(certified_values[2], ")" => ""))
+function datasets()
+    out = OrderedDict{String,Vector{String}}()
+    for dir in ["anova", "mcmc", "nonlinear", "regression", "univariate"]
+        dsets = map(x -> replace(x, ".dat" => ""), readdir(depsdir(dir)))
+        append!(get!(out, dir, String[]), dsets) 
+    end
+    return out
 end
 
+#-----------------------------------------------------------------------------# TestCase
+struct TestCase
+    name::String
+    localfile::String
+    url::String
+    format::String 
+    procedure::String
+    reference::String 
+    data_description::String 
+    model::String
+    data::String
+    certified_values::Vector
+end
+function TestCase(name::String)
+    name = endswith(name, ".dat") ? name : name * ".dat"
+    if isfile(depsdir("anova", name))
+        url = "https://www.itl.nist.gov/div898/strd/anova/$name"
+        localfile = depsdir("anova", name)
+    elseif isfile(depsdir("mcmc", name))
+        url = "https://www.itl.nist.gov/div898/strd/mcmc/$name"
+        localfile = depsdir("mcmc", name)
+    elseif isfile(depsdir("nonlinear", name))
+        url = "https://www.itl.nist.gov/div898/strd/nls/data/LINKS/DATA/$name"
+        localfile = depsdir("nonlinear", name)
+    elseif isfile(depsdir("regression", name))
+        url = "https://www.itl.nist.gov/div898/strd/lls/data/LINKS/DATA/$name"
+        localfile = depsdir("regression", name)
+    elseif isfile(depsdir("univariate", name))
+        url = "https://www.itl.nist.gov/div898/strd/univ/data/$name"
+        localfile = depsdir("univariate", name)
+    else
+        error("No dataset with the name \"$name\" was found.")
+    end
+    lines = readlines(localfile)
+    TestCase(name, localfile, url, get_format(lines), get_procedure(lines), get_reference(lines), 
+        get_data_description(lines), get_model(lines), get_data(lines), get_certified_values(lines))
+end
+
+
+function extract_section(lines, heading::String; leftalign=true, op=findfirst)
+    i = op(startswith(heading * ":"), lines)
+    isnothing(i) && return "Univariate"
+    line = replace(lines[i], "$heading:" => "")
+    j = findnext(!startswith(r" |-"), lines, i + 1)
+    out = isnothing(j) ? vcat(line, lines[i+1:end]) : vcat(line, lines[i+1:j-1])
+    out = leftalign ? lstrip.(out) : out
+    replace(join(out, '\n'), "$heading:"=>"")
+end
+
+function get_format(lines)
+    extract_section(lines, "File Format")
+end
+
+function get_procedure(lines)
+   extract_section(lines, "Procedure")
+end
+
+function get_reference(lines)
+    extract_section(lines, "Reference")
+end
+
+function get_data_description(lines)
+    extract_section(lines, "Data")
+end
+
+function get_model(lines)
+    extract_section(lines, "Model")
+end
+
+function get_data(lines)
+    replace(extract_section(lines, "Data"; op=findlast), r"[ \t]+" => ',')
+end
+
+function get_certified_values(lines)
+    out = []
+    for T in [AnovaTable, Coefficients, RSquared, ResidualStandardError, MCMCStats, UnivariateStats]
+        x = extract(T, lines)
+        isnothing(x) || push!(out, x)
+    end
+    return out
+end
+
+function Base.show(io::IO, t::TestCase)
+    printstyled(io, "Test Case\n", color=:light_cyan, bold=true)
+    for (k,v) in [
+            "LocalFile" => t.localfile, 
+            "URL      " => t.url,
+            "Procedure" => t.procedure,
+        ]
+        printstyled(io, "    $k: ")
+        printstyled(io, v, color=:light_green)
+        println(io)
+    end
+    for (k,v) in [
+            # "File Format" => t.format,
+            "Data Description" => t.data_description,
+            "Model" => t.model,
+            "Data (CSV)" => "$(sum(x -> x == '\n', t.data)) Lines"
+        ]
+        v2 = split(string(v), '\n')
+        printstyled(io, "    $k: \n")
+        for val in v2 
+            printstyled(io, "        $val\n", color=:light_green)
+        end
+    end
+    printstyled(io, "    Certified Values:\n")
+    for val in t.certified_values 
+        for row in split(string(val), '\n')
+            printstyled(io, "        $row\n", color=:light_green)
+        end
+    end
+end
+
+
+#----------------------------------------------------# AnovaTable (anova, regression)
+struct AnovaTable 
+    model::NamedTuple{(:df, :ss, :ms), Tuple{Int, Float64, Float64}}
+    residual::NamedTuple{(:df, :ss, :ms), Tuple{Int, Float64, Float64}}
+    f_statistic::Float64
+end
+function Base.show(io::IO, t::AnovaTable)
+    m, r = t.model, t.residual
+    d = DataFrame(source=["Model", "Residual"], df=[m.df, r.df], ss=[m.ss, r.ss], ms=[m.ms, r.ms])
+    printstyled(io, "AnovaTable                           F = $(t.f_statistic)\n", bold=true)
+    io2 = IOBuffer()
+    show(io2, d; summary=false, eltypes=false, show_row_number=false)
+    for row in split(String(take!(io2)), '\n')
+        println(io, "    $row")
+    end
+end
+
+function extract(::Type{AnovaTable}, lines)
+    # anova
+    i = findfirst(startswith("Between"), lines)
+    if !isnothing(i)
+        _, _, df, ss, ms, f = split(lines[i])
+        _, _, df2, ss2, ms2 = split(lines[i+1])
+        return AnovaTable(
+            (df = Parsers.parse(Int, df), ss = Parsers.parse(Float64, ss), ms=Parsers.parse(Float64, ms)),
+            (df = Parsers.parse(Int, df2), ss = Parsers.parse(Float64, ss2), ms=Parsers.parse(Float64, ms2)),
+            Parsers.parse(Float64, f)
+        )
+    end
+    # regression
+    i = findfirst(x -> occursin("Certified Analysis of Variance Table", x), lines)
+    if !isnothing(i)
+        _, df, ss, ms, f = split(lines[i+5])
+        _, df2, ss2, ms2 = split(lines[i+6])
+        return AnovaTable(
+            (df = Parsers.parse(Int, df), ss = Parsers.parse(Float64, ss), ms=Parsers.parse(Float64, ms)),
+            (df = Parsers.parse(Int, df2), ss = Parsers.parse(Float64, ss2), ms=Parsers.parse(Float64, ms2)),
+            Parsers.parse(Float64, f)
+        )
+    end
+    return nothing
+end
+
+#----------------------------------------------------# Coefficients (nonlinear, regression)
+struct Coefficients 
+    β::Vector{Float64}
+    β_std_err::Vector{Float64}
+end
+function Base.show(io::IO, c::Coefficients)
+    println(io, "Coefficients")
+    d = DataFrame(β = c.β, se = c.β_std_err)
+    io2 = IOBuffer()
+    show(io2, d; summary=false, eltypes=false, show_row_number=false)
+    for row in split(String(take!(io2)), '\n')
+        println(io, "    $row")
+    end
+end
+
+function extract(::Type{Coefficients}, lines)
+    # regression 
+    i = findfirst(x -> occursin("Certified Regression Statistics", x), lines)
+    if !isnothing(i) 
+        β, βse = Float64[], Float64[]
+        j = 5
+        line = lines[i + j]
+        while startswith(lstrip(line), 'B')
+            _, βi, βsei = split(line)
+            push!(β, Parsers.parse(Float64, βi))
+            push!(βse, Parsers.parse(Float64, βsei))
+            j += 1 
+            line = lines[i + j]
+        end
+        return Coefficients(β, βse)
+    end
+    return nothing
+end
+
+#----------------------------------------------------# RSquared (anova, regression)
+struct RSquared
+    r2::Float64 
+end 
+Base.show(io::IO, r::RSquared) = print(io, "R-Squared: ", r.r2)
+function extract(::Type{RSquared}, lines)
+    i = findfirst(x -> occursin("R-Squared", x), lines)
+    if !isnothing(i)
+        line = lines[i]
+        j = occursin("Certified", line) ? 3 : 2
+        return RSquared(Parsers.parse(Float64, split(lines[i])[j]))
+    end
+    return nothing
+end
+
+#----------------------------------------------------# ResidualStandardError (anova, regression, nonlinear)
+struct ResidualStandardError 
+    resid_std_err::Float64
+end
+Base.show(io::IO, r::ResidualStandardError) = print(io, "Residual Standard Error: ", r.resid_std_err)
+function extract(::Type{ResidualStandardError}, lines)
+    i = findfirst(x -> occursin("Residual", x), lines)
+    if !isnothing(i)
+        return ResidualStandardError(Parsers.parse(Float64, split(lines[i+1])[end]))
+    end
+    return nothing
+end
+
+#-----------------------------------------------------------------------------# MCMCStats 
+struct MCMCStats 
+    μ_posterior::NamedTuple{(:m, :s, :q), Tuple{Float64, Float64, Vector{Float64}}}
+    σ_posterior::NamedTuple{(:m, :s, :q), Tuple{Float64, Float64, Vector{Float64}}}
+end
+function Base.show(io::IO, m::MCMCStats)
+    io2 = IOContext(io, :compact=>true)
+    println(io2, "MCMCStats")
+    println(io2, "    μ Posterior: |", m.μ_posterior)
+    println(io2, "    σ Posterior: |", m.σ_posterior)
+end
+function extract(::Type{MCMCStats}, lines)
+    i = findfirst(x -> occursin("Posterior mean of mu", x), lines)
+    if !isnothing(i) 
+        return MCMCStats(
+            (
+                m = Parsers.parse(Float64, split(lines[i])[end]),
+                s = Parsers.parse(Float64, split(lines[i+1])[end]),
+                q = [
+                    Parsers.parse(Float64, split(lines[i+2])[end]),
+                    Parsers.parse(Float64, split(lines[i+3])[end]),
+                    Parsers.parse(Float64, split(lines[i+4])[end]),
+                ]
+            ),
+            (
+                m = Parsers.parse(Float64, split(lines[i+6])[end]),
+                s = Parsers.parse(Float64, split(lines[i+7])[end]),
+                q = [
+                    Parsers.parse(Float64, split(lines[i+8])[end]),
+                    Parsers.parse(Float64, split(lines[i+9])[end]),
+                    Parsers.parse(Float64, split(lines[i+10])[end]),
+                ]
+            )
+        )
+    end
+    return nothing
+end
+
+#----------------------------------------------------# UnivariateStats
+struct UnivariateStats 
+    mean::Float64 
+    std::Float64 
+    autocor::Float64
+    is_exact::Bool
+    nobs::Int
+end
+function Base.show(io::IO, s::UnivariateStats)
+    print(io, "UnivariateStats: m=$(s.mean), s=$(s.std), a=$(s.autocor), n=$(s.nobs) | exact=$(s.is_exact)")
+end
+function extract(::Type{UnivariateStats}, lines)
+    i = findfirst(x -> occursin("Sample Mean", x), lines)
+    @info i
+    if !isnothing(i)
+        s1 = split(lines[i])
+        j = s1[end] == "(exact)" ? 1 : 0
+        m = Parsers.parse(Float64, s1[end-j])
+        s = Parsers.parse(Float64, split(lines[i+1])[end-j])
+        a = Parsers.parse(Float64, split(lines[i+2])[end-j])
+        nobs = Parsers.parse(Int, split(lines[i+4])[end])
+        return UnivariateStats(m, s, a, j == 1, nobs)
+    end
+    return nothing
+end
+
+#-----------------------------------------------------------------------------# anova 
+datasets = [
+    (name="AtmWtAg", )
+]
+
+# #-----------------------------------------------------------------------------# sections
+# function sections(file::String)
+#     lines = filter(!isempty, readlines(file))
+#     out = OrderedDict{String,Any}()
+#     idxs = findall(x -> occursin(':', x), lines)
+#     push!(idxs, length(lines) + 1)
+#     for idx in 2:length(idxs)
+#         i, j = idxs[idx-1], idxs[idx] - 1
+#         key = replace(lines[i], r":.*" => "")
+#         key2 = haskey(out, key) ? "$key 2" : key
+#         out[key2] = lstrip(replace(join(lines[i:j], '\n'), "$key:" => ""))
+#     end
+#     nm = out["Dataset Name"]
+#     out["Dataset Name"] = nm[1:findfirst(' ', nm)-1]
+#     ff = out["File Format"]
+#     idx = findall(r"(?<=\().*(?=\))", ff)
+#     out["File Format"] = OrderedDict(
+#         "Certified Values Lines" => parse.(Int, split(ff[idx[1]])[[2,4]]),
+#         "Data Lines" => parse.(Int, split(ff[idx[2]])[[2,4]])
+#     )
+#     if out["Procedure"] == "Analysis of Variance"
+#         process_for_anova!(out)
+#     end
+#     out["Data 2"] = CSV.read(IOBuffer(out["Data 2"]), DataFrame, delim=' ', ignorerepeated=true)
+#     return out
+# end
+
+# for f in [:anova, :mcmc, :nonlinear, :regression, :univariate]
+#     @eval begin
+#         function $f(dataset::String)
+#             dir = string($(QuoteNode(f)))
+#             file = endswith(dataset, ".dat") ? dataset : "$dataset.dat"
+#             path = abspath(joinpath(@__DIR__, "..", "deps", dir, file))
+#             isfile(path) ? sections(path) : error("$path Not Found.")
+#         end
+#         $f(i::Int) = $f(readdir(joinpath(@__DIR__, "..", "deps", string($(QuoteNode(f)))))[i])
+#     end
+# end
 
 
 end
