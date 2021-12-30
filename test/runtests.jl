09185a5eac28ff0915ded7fcd200b78d4cb870de
joshday <emailjoshday@gmail.com>
2021-12-30 12:20:44 -0500

    test comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,7 @@ using Test
             for vi in v 
                 println("Loading $vi...")
                 t = NISTStatisticalReferenceData.TestCase(vi)
+                # check that at least one certified value was found in every dat file.
                 @test !isempty(t.certified_values)
             end
         end
